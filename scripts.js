b833f9614117bbb7261fc542653258e1da282756
Lucas Benediht Caldeira <110697669+Lucas-Benediht@users.noreply.github.com>
2023-02-16 20:23:19 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,49 @@
+function verificar (){
+    var date = new Date()
+    var ano = date.getFullYear() // Isso aqui é pra puxar o ano em 4 dígitos
+    var fano= document.getElementById('txtano')
+    var res = document.getElementById ('res')
+    if (fano.value.length == 0 || Number (fano.value) >ano ){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }else{
+        var fsex= document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img') // Cria tag img
+        img.setAttribute('id','foto')
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if (idade >=0 && idade <10){
+                //Criança
+                img.setAttribute('src', 'bebemasculino.jpg')
+            }else if (idade <21){
+                //Jovem
+                img.setAttribute('src', 'adolescentem.jpg')
+            }else if (idade <50){
+                //Adulto
+                img.setAttribute('src', 'adultomasculino.jpg')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'idosomasculino.jpg')
+            }
+        }else if (fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >=0 && idade <10){
+                //Criança
+                img.setAttribute('src', 'bebefeminino.jpg')
+            }else if(idade < 21){
+                //Jovem
+                img.setAttribute('src', 'adolescentefeminino.jpg')
+            }else if (idade <50){
+                //Adulto
+                img.setAttribute('src', 'mulheradulta.jpg')
+            }else{
+                //Idoso
+                img.setAttribute('src', 'idosafeminino.jpg')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)//adicionar o elemento img
+    }
+
+}
