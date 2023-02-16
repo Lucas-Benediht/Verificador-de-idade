b4df37844d6a77f5c714e86c0c0d95228f9705cf
Lucas Benediht Caldeira <110697669+Lucas-Benediht@users.noreply.github.com>
2023-02-16 20:51:24 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function verificar (){
     var fano= document.getElementById('txtano')
     var res = document.getElementById ('res')
     if (fano.value.length == 0 || Number (fano.value) >ano ){
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+        window.alert('[ERRO❌] Verifique os dados e tente novamente!')
     }else{
         var fsex= document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
